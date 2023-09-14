8a6fdef3c618becd93e52e0b428f5cb31a3cbfb2
Edwin Cardenas <eacardenase@unal.edu.co>
2023-09-14 18:01:03 -0500

    feat: add pricing route and page

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,12 +1,31 @@
 import styles from '@/components/NavBar.module.css';
 import { ActiveLink } from '@/components/ActiveLink';
 
+const menuItems = [
+  {
+    text: 'Home',
+    href: '/',
+  },
+  {
+    text: 'About',
+    href: '/about',
+  },
+  {
+    text: 'Contact',
+    href: '/contact',
+  },
+  {
+    text: 'Pricing',
+    href: '/pricing',
+  },
+];
+
 export const NavBar = () => {
   return (
     <nav className={styles['menu-container']}>
-      <ActiveLink text="Home" href="/" />
-      <ActiveLink text="About" href="/about" />
-      <ActiveLink text="Contact" href="/contact" />
+      {menuItems.map((navItem, index) => (
+        <ActiveLink key={index} text={navItem.text} href={navItem.href} />
+      ))}
     </nav>
   );
 };
