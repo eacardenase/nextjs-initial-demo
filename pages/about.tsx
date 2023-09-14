e1707d372914b8e87eb4d84e844f0f77ed9aa7f7
Edwin Cardenas <eacardenase@unal.edu.co>
2023-09-13 21:28:00 -0500

    feat: add about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -2,24 +2,24 @@ import Head from 'next/head';
 
 import styles from '@/styles/Home.module.css';
 
-export default function HomePage() {
+export default function AboutPage() {
   return (
     <>
       <Head>
-        <title>Home - Edwin</title>
-        <meta name="description" content="Home Page" />
+        <title>About - Edwin</title>
+        <meta name="description" content="About Page" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
         <h1>
           {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
-          Ir a <a href="/about">About</a>
+          Ir a <a href="/">Home</a>
         </h1>
         <div className={styles.description}>
           <p>
             Get started by editing{' '}
-            <code className={styles.code}>pages/index.tsx</code>
+            <code className={styles.code}>pages/about.tsx</code>
           </p>
         </div>
       </main>
